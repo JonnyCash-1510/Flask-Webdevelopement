db7a0b3cf0686d36662e7e419731b98105210b8f
Jonny Cash <jonathan.kaesch@gmx.de>
2023-05-21 10:03:22 +0200

    Second Commit

---
+++ website/views.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, flash, render_template, request
+from flask_login import login_required,  current_user
+from .models import Note
+from . import db
+
+
+views = Blueprint("views", __name__)
+
+@views.route('/', methods = ["GET", "POST"])
+@login_required
+def home():
+   if request.method == "POST":
+      note = request.form.get("note")
+      if len(note) < 1:        # type: ignore
+         flash("Note is to short.", category="error")
+      else:
+         new_note = Note(data=note, user_id = current_user.id) # type: ignore
+         db.session.add(new_note)
+         db.session.commit()
+         flash("Note added!", category="success")
+         
+   return render_template("home.html", user = current_user)
+

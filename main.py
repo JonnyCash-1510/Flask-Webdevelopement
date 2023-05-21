572d4b6ab172f3e4447c972a28b7a2ef315f373b
Jonny Cash <jonathan.kaesch@gmx.de>
2023-05-21 10:08:54 +0200

    Test Commit 1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from website import create_app
 
 app = create_app()
 
-
+#Ich mache jetzt einen Kommentar dazu und das kann man dann auf GitHub sehen!
 
 if __name__ == "__main__":
-   app.run(debug=True) #hello
+   app.run(debug=True)
